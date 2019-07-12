8131dbeb1c68f29ae87d0351577b758c54bce718
Влад <vladyslav.andrus@gmail.com>
2019-07-12 15:08:46 +0200

    last update

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Влад on 7/12/19.
+//  Copyright © 2019 Влад. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player  {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
